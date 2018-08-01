f55eb111dd96d4fcb0f3b52e5efb71daab316ce5
Alan Yoho <alanyoh@gmail.com>
2018-08-01 19:13:27 -0400

    boilerplate added, three views made

---
+++ components/LandingView.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+export default class LandingView extends React.Component {
+    static navigationOptions = {
+        // title: 'Profile Screen',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <Button
+                    title="Continue With Facebook"
+                    onPress={() =>
+                        navigate('AccountSetupView', { name: 'Jane' })
+                    }
+                />
+            </View>
+        );
+    }
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
