cee66c986241257f462539ce030dbc4938e2515c
Alan Yoho <alanyoh@gmail.com>
2018-08-05 16:38:49 -0400

    cleaned up UI

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Linking } from 'react-native';
-import { LandingView, AccountSetupView, ConnectSpotifyView, ListeningRoomView, AuthenticateSpotifyView } from "./src/components"
+import { AccountSetupView, ListeningRoomView, AuthenticateSpotifyView } from "./src/components"
 import { createStackNavigator } from 'react-navigation';
 
 const App = createStackNavigator({
-  // LandingView: { screen: LandingView },
   AuthenticateSpotifyView: { screen: AuthenticateSpotifyView },
-  ConnectSpotifyView: { screen: ConnectSpotifyView },
   AccountSetupView: { screen: AccountSetupView },
   ListeningRoomView: { screen: ListeningRoomView }
 
