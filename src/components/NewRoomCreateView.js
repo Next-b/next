5a97ecfd228e3d62d035a87eac480255f8845d86
Ray Lee <raylee971227@gmail.com>
2018-08-06 12:36:31 -0400

    can add listening room

--- src/components/NewRoomCreateView.js
+++ src/components/NewRoomCreateView.js
@@ -1,22 +1,50 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
-
-
+import {FormLabel, FormInput} from 'react-native-elements'
+import firestore from "../../server/fireBase"
 
 export default class NewRoomCreateView extends Component {
   constructor() {
     super()
     this.state = {
       roomName: '',
-
+      
     }
+    this.createRoom = this.createRoom.bind(this)
+    this.handlePress = this.handlePress.bind(this)
+  }
+  componentDidMount() {
+    this.input.focus()
   }
 
+
+  createRoom() {
+    var userData = this.props.navigation.state.params.userData
+    firestore.collection("Rooms").add({
+      roomName: this.state.roomName,
+      owner: {
+        name: userData.display_name,
+        image: userData.images[0].url
+      }
+    })
+  }
+  handlePress() {
+    const {navigate} = this.props.navigation
+    this.createRoom();
+    navigate('FindCreateListeningRoomView')
+  }
   render(){
     return(
       <View>
-        <Text>NEW ROOM VIEW</Text>
-        {console.log(this.props.navigation.state.params.userData)}
+        <FormLabel>Room Name: </FormLabel>
+        <FormInput 
+          ref={input => this.input = input} 
+          inputStyle={{textAlign: 'center', justifyContent: 'center'}}
+          defaultValue={this.state.roomName}
+          placeholder="Add room name"
+          onChangeText={name => (this.setState({roomName: name}))}
+        /> 
+        <Text onPress={this.handlePress}>Create Room</Text>
       </View>
     )
   }
